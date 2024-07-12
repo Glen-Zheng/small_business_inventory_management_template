923995195619463d633974cc7ea4bc4ffdae0161
Glen <zhengglen200@gmail.com>
2024-07-12 02:35:46 -0400

    Added database connection testing:

--- utils/database.js
+++ utils/database.js
@@ -3,6 +3,7 @@ import mysql from "mysql2/promise";
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port: process.env.DB_PORT,
   password: process.env.DB_PASS,
   database: process.env.DB_SCHEMA,
   waitForConnections: true,
@@ -12,5 +13,17 @@ const pool = mysql.createPool({
     ca: process.env.DB_SSL_CA,
   },
 });
+
+async function testConnection() {
+  try {
+    const connection = await pool.getConnection();
+    console.log("Database connected successfully");
+    connection.release();
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+  }
+}
+
+testConnection();
 
 export default pool;
